ac99581110c2719966e505197f99a5c29d8d6537
Poornima Devi Cherukuri <147487916+poornimacherukuri@users.noreply.github.com>
2024-07-31 15:56:05 +0530

    import java.util.Scanner; public class Avgweight{ public static void main(String[] args) { int a,b,c; Scanner read=new Scanner(System.in); a=read.nextInt(); b=read.nextInt(); c=read.nextInt(); System.out.println(3*a-b-c); } }

---
+++ Average_weight.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Avgweight{
+public static void main(String[] args)
+{
+int a,b,c;
+Scanner read=new Scanner(System.in);
+a=read.nextInt();
+b=read.nextInt();
+c=read.nextInt();
+System.out.println(3*a-b-c);
+}
+}
