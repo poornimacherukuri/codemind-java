3b888bf47128c76c411ef1761569ec8b4aac874f
Poornima Devi Cherukuri <147487916+poornimacherukuri@users.noreply.github.com>
2024-07-31 15:16:53 +0530

    import java.util.Scanner; public class Frames{ public static void main(String[] args) { int x,y,z; Scanner read=new Scanner(System.in); x=read.nextInt(); y=read.nextInt(); z=read.nextInt(); System.out.println(2*(x+y)*z); } }

---
+++ Frames.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Frames{
+public static void main(String[] args)
+{
+int x,y,z;
+Scanner read=new Scanner(System.in);
+x=read.nextInt();
+y=read.nextInt();
+z=read.nextInt();
+System.out.println(2*(x+y)*z);
+}
+}
