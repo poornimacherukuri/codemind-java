a4ec8a7ae5a979a36962000ff3bce4b9daee3254
Poornima Devi Cherukuri <147487916+poornimacherukuri@users.noreply.github.com>
2024-07-31 15:29:17 +0530

    import java.util.Scanner; public class KingTours{ public static void main(String[] args) { int a,b,c; Scanner read=new Scanner(System.in); a=read.nextInt(); b=read.nextInt(); c=(a*5)+(b*7); System.out.println(c); } }

---
+++ King_Tours_.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class KingTours{
+public static void main(String[] args)
+{
+int a,b,c;
+Scanner read=new Scanner(System.in);
+a=read.nextInt();
+b=read.nextInt();
+c=(a*5)+(b*7);
+System.out.println(c);
+}
+}
