01e9f3e9136ef5343a9d2bfb888cc8c6669dc223
Poornima Devi Cherukuri <147487916+poornimacherukuri@users.noreply.github.com>
2024-07-31 15:33:52 +0530

    import java.util.Scanner; public class Example{ public static void main(String[] args) { int s; Scanner read=new Scanner(System.in); s=read.nextInt(); System.out.println(s*s+" "+4*s); } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Example{
+public static void main(String[] args)
+{
+int s;
+Scanner read=new Scanner(System.in);
+s=read.nextInt();
+System.out.println(s*s+" "+4*s);
+}
+}
