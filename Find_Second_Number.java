c4034dd23c8479fb84561b567604703824c1a3b7
Poornima Devi Cherukuri <147487916+poornimacherukuri@users.noreply.github.com>
2024-07-31 15:45:46 +0530

    import java.util.Scanner; public class SecondNum{ public static void main(String[] args) { int a,b,x; Scanner read=new Scanner(System.in); a=read.nextInt(); b=read.nextInt(); x=2*a-b; System.out.println(x); } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SecondNum{
+public static void main(String[] args)
+{
+int a,b,x;
+Scanner read=new Scanner(System.in);
+a=read.nextInt();
+b=read.nextInt();
+x=2*a-b;
+System.out.println(x);
+}
+}
